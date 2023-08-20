8ab964cd3227148cb50a8d8bd54d284fc693e979
HyeonTee <118068099+HyeonTee@users.noreply.github.com>
2023-08-21 01:12:52 +0900

    Time: 900 ms (83.29%), Space: 32.5 MB (25.99%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -4,11 +4,11 @@ class Solution:
         second = float('inf')
         
         for num in nums:
-            if first >= num:
+            if first >= num: # 가장 작은 수
                 first = num
-            elif second >= num:
+            elif second >= num: # 두번째로 작은 수
                 second = num
-            else:
+            else: # 세번째로 작은 수 존재하는 경우
                 return True
         
         return False
