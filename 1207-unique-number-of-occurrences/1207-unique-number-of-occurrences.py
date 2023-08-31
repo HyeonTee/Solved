4c0ed7ebef53a430c853912165d26d01d819efba
HyeonTee <118068099+HyeonTee@users.noreply.github.com>
2023-08-31 19:25:20 +0900

    Time: 51 ms (38.65%), Space: 16.4 MB (51.99%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,12 +1,8 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        memo = {}
-        for a in arr:
-            if a not in memo:
-                memo[a] = 1
-            else:
-                memo[a] += 1
+        occ_list = []
         
-        occurs = list(memo.values())
-        
-        return len(occurs) == len(set(occurs))
+        for i in set(arr):
+            occ_list.append(arr.count(i))
+            
+        return len(set(occ_list)) == len(set(arr))
