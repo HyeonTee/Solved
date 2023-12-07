f9696b2f2f084d05a18115aa3a00cb38a13ad0a0
HyeonTee <118068099+HyeonTee@users.noreply.github.com>
2023-12-07 23:32:25 +0900

    Time: 1055 ms (63.71%), Space: 20.5 MB (51.36%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,28 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
-        memo = {}
-        for num in nums:
-            if num in memo:
-                memo[num] += 1
-            else:
-                memo[num] = 1
-        
-        if 0 in memo and memo[0] >= 3:
-            ans.add((0,0,0))
-        
-        nums = list(set(nums))
-        n = len(nums)
+        ans = []
+        nums.sort()
 
-        for i in range(n):
-            for j in range(i+1, n):
-                if -(nums[i]+nums[j]) in memo:
-                    if -(nums[i]+nums[j]) != nums[i] and -(nums[i]+nums[j]) != nums[j]:
-                        ans.add(tuple(sorted([-(nums[i]+nums[j]), nums[i], nums[j]])))
-                    
-                    if -(nums[i]+nums[j]) == nums[i] or -(nums[i]+nums[j]) == nums[j]:
-                        if memo[-(nums[i]+nums[j])] > 1:
-                            ans.add(tuple(sorted([-(nums[i]+nums[j]), nums[i], nums[j]])))
-        
-        
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    ans.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+
         return ans
