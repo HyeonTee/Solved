a1679a91ed5e55e55226bce3f6e066c36a92542b
HyeonTee <118068099+HyeonTee@users.noreply.github.com>
2025-03-02 16:41:21 +0900

    [Gold IV] Title: 동전 1, Time: 208 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+dp = [0] * (k + 1)
+dp[0] = 1
+coins = []
+for _ in range(n):
+	coin = int(input())
+	coins.append(coin)
+
+for coin in coins:
+	for i in range(coin, k+1):
+		dp[i] += dp[i-coin]
+
+print(dp[k])
