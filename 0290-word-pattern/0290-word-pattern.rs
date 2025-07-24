f72ad6d9131c8f073300f6b83a4b414b0b9cc697
김현태 <118068099+HyeonTee@users.noreply.github.com>
2025-07-25 08:58:14 +0900

    Time: 0 ms (100%), Space: 2.4 MB (1.23%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.rs
+++ 0290-word-pattern/0290-word-pattern.rs
@@ -2,30 +2,28 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn word_pattern(pattern: String, s: String) -> bool {
-        let pattern_chars = pattern.chars().collect::<Vec<char>>();
-        let s_words = s.split_whitespace().collect::<Vec<&str>>();
+        let pattern_chars = pattern.chars().collect::<Vec<_>>();
+        let s_words = s.split_whitespace().collect::<Vec<_>>();
 
         if pattern_chars.len() != s_words.len() {
             return false;
         }
 
-        let mut result = true;
-        let mut map: HashMap<char, &str> = HashMap::new();
-        for i in 0..pattern_chars.len() {
-            if map.contains_key(&pattern_chars[i]) {
-                if map.get(&pattern_chars[i]).unwrap() != &s_words[i] {
-                    result = false;
-                    break;
+        let mut char_to_word = HashMap::new();
+        let mut word_to_char = HashMap::new();
+
+        for (ch, word) in pattern_chars.iter().zip(s_words.iter()) {
+            match (char_to_word.get(ch), word_to_char.get(word)) {
+                (Some(w), _) if w != word => return false,
+                (_, Some(c)) if c != ch => return false,
+                (None, None) => {
+                    char_to_word.insert(*ch, *word);
+                    word_to_char.insert(*word, *ch);
                 }
-            } else {
-                if map.values().any(|&v| v == s_words[i]) {
-                    result = false;
-                    break;
-                }
-                map.insert(pattern_chars[i], s_words[i]);
+                _ => {}
             }
         }
 
-        result
+        true
     }
 }
