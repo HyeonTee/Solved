121a3ec8a004b48582e8cb679a72a0de3ba41f0a
김현태 <118068099+HyeonTee@users.noreply.github.com>
2025-07-31 17:51:49 +0900

    Time: 5 ms (78.68%), Space: 64.2 MB (58.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,18 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    if (x < 0) return false
-    const numList = []
-    while (x !== 0) {
-        numList.push(x % 10)
-        x = Math.floor(x / 10)
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reversed = 0;
+
+    while (x > reversed) {
+        reversed = reversed * 10 + x % 10;
+        x = Math.floor(x / 10);
     }
 
-    for (let i = 0; i < numList.length; i++) {
-        if (numList[i] !== numList[numList.length - i - 1]) {
-            return false
-        }
-    }
-
-    return true
+    return x === reversed || x === Math.floor(reversed / 10);
 };
