0f2d2b502e5c779f0067b90886a9b4857635c501
김현태 <118068099+HyeonTee@users.noreply.github.com>
2025-08-20 16:31:47 +0900

    [Silver II] Title: 에디터, Time: 352 ms, Memory: 44456 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,40 +1,57 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder(br.readLine());
-        int cursor = sb.length();
-
+        String init = br.readLine();
         int m = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+
+        // 최대 길이 = 초기 길이 + 최대 삽입 횟수 + 여유
+        int cap = init.length() + m + 8;
+        char[] left = new char[cap];
+        char[] right = new char[cap];
+        int lsz = init.length(); // left stack size (cursor is at lsz)
+        int rsz = 0;             // right stack size
+
+        // 초기 문자열을 왼쪽 스택에 채워두기
+        init.getChars(0, init.length(), left, 0);
+
+        String line;
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-            switch (command) {
-                case "L":
-                    if (cursor > 0) cursor--;
-                    break;
-                case "D":
-                    if (cursor < sb.length()) cursor++;
-                    break;
-                case "B":
-                    if (cursor > 0) {
-                        sb.deleteCharAt(cursor - 1);
-                        cursor--;
+            line = br.readLine();
+            char cmd = line.charAt(0);
+
+            switch (cmd) {
+                case 'L':
+                    if (lsz > 0) {
+                        right[rsz++] = left[--lsz];
                     }
                     break;
-                case "P":
-                    String charToInsert = st.nextToken();
-                    sb.insert(cursor, charToInsert);
-                    cursor++;
+                case 'D':
+                    if (rsz > 0) {
+                        left[lsz++] = right[--rsz];
+                    }
+                    break;
+                case 'B':
+                    if (lsz > 0) {
+                        lsz--;
+                    }
+                    break;
+                case 'P':
+                    // line 형식: "P x"
+                    char ch = line.charAt(2);
+                    left[lsz++] = ch;
                     break;
             }
         }
 
-        System.out.println(sb);
+        // 결과 빌드: left 그대로 + right 역순
+        StringBuilder sb = new StringBuilder(lsz + rsz);
+        sb.append(left, 0, lsz);
+        for (int i = rsz - 1; i >= 0; i--) sb.append(right[i]);
+
+        System.out.print(sb);
     }
 }
