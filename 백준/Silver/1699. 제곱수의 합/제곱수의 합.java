b9fb33d98b34e25f45872611b5f7aca11044d82a
김현태 <118068099+HyeonTee@users.noreply.github.com>
2025-09-05 09:47:35 +0900

    [Silver II] Title: 제곱수의 합, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,25 +1,34 @@
 import java.io.*;
 
 public class Main {
+    static boolean isSquare(int x) {
+        int r = (int) Math.sqrt(x);
+        return r * r == x;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        if (n < 4) {
-            System.out.println(n);
+
+        if (isSquare(n)) {
+            System.out.println(1);
+            return;
+        }
+
+        for (int i = 1; i * i <= n; i++) {
+            if (isSquare(n - i * i)) {
+                System.out.println(2);
+                return;
+            }
+        }
+
+        int m = n;
+        while (m % 4 == 0) m /= 4;
+        if (m % 8 == 7) {
+            System.out.println(4);
         } else {
-            int[] dp = new int[n+1];
-            for (int i = 0; i <= n; i++) {
-                dp[i] = i;
-            }
-
-            for (int i = 4; i <= n; i++) {
-                for (int j = 1; j * j <= i; j++) {
-                    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
-                }
-            }
-
-            System.out.println(dp[n]);
+            System.out.println(3);
         }
     }
 }
