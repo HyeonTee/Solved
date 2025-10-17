23a6c7181c316a70be7c7a2f40186c0172c9ff7f
김현태 <118068099+HyeonTee@users.noreply.github.com>
2025-10-17 11:17:47 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,49 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static boolean[] visited;
-    static int n, m;
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n];
-        visited = new boolean[n];
+        int[] c = new int[m];
+        for (int i = 0; i < m; i++) c[i] = i + 1;
 
-        dfs(0);
-        System.out.println(sb);
-    }
-
-    static void dfs(int length) {
-        if (length == m) {
-            for (int i = 0; i < m; i++) {
-                sb.append(arr[i]).append(' ');
-            }
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            for (int x : c) sb.append(x).append(' ');
             sb.append('\n');
-            return;
+
+            int i = m - 1;
+            while (i >= 0 && c[i] == n - (m - 1 - i)) i--;
+            if (i < 0) break;
+
+            c[i]++;
+            for (int j = i + 1; j < m; j++) c[j] = c[j - 1] + 1;
         }
 
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                if (length > 0) {
-                    if (arr[length - 1] < i+1) {
-                        visited[i] = true;
-                        arr[length] = i+1;
-                        dfs(length + 1);
-                        visited[i] = false;
-                    }
-                } else {
-                    visited[i] = true;
-                    arr[length] = i+1;
-                    dfs(length + 1);
-                    visited[i] = false;
-                }
-            }
-        }
+        System.out.print(sb);
     }
 }
