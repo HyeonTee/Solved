f33211da9639f5a35403b4cbb4c9dc0ffe220bad
김현태 <118068099+HyeonTee@users.noreply.github.com>
2025-10-29 11:28:50 +0900

    [Gold V] Title: 합분해, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[k+1][n+1];
+        for (int i = 1; i <= n; i++){
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= k; i++){
+            dp[i][0] = 1;
+            for (int j = 1; j <= n; j++){
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j]) % 1000000000;
+            }
+        }
+
+        System.out.println(dp[k][n]);
+    }
+}
