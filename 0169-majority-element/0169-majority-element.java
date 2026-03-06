743c9edf479e4b3924abe439e99e86d4312e9075
김현태 <118068099+HyeonTee@users.noreply.github.com>
2026-03-06 12:55:45 +0900

    Time: 12 ms (33.29%), Space: 52.6 MB (90.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,22 +2,16 @@ import java.util.*;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
+        Map<Integer, Integer> countMap = new HashMap<>();
 
-        int ans = nums[0];
-        int cnt = 1;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                cnt++;
-                if (cnt > nums.length >> 1) {
-                    ans = nums[i];
-                    break;
-                }
-            } else {
-                cnt = 1;
+        for (int num : nums) {
+            int count = countMap.getOrDefault(num, 0) + 1;
+            if (count > nums.length / 2) {
+                return num;
             }
+            countMap.put(num, count);
         }
 
-        return ans;
+        return -1;
     }
 }
