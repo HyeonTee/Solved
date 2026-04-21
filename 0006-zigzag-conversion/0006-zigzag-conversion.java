4a042a656e628c581172916ca8b2cabcdd74a9e3
김현태 <118068099+HyeonTee@users.noreply.github.com>
2026-04-21 17:48:54 +0900

    Time: 5 ms (56.06%), Space: 47.2 MB (17.79%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,28 +1,32 @@
 class Solution {
     public String convert(String s, int numRows) {
-        if (numRows == 1) {
+       if (numRows == 1 || numRows >= s.length()) {
             return s;
         }
 
-        StringBuilder[] stacks = new StringBuilder[numRows];
+        int idx = 0, d = 1;
+        List<Character>[] rows = new ArrayList[numRows];
         for (int i = 0; i < numRows; i++) {
-            stacks[i] = new StringBuilder();
-        }
-        for (int i = 0; i < s.length(); i++) {
-            stacks[cycle(i, numRows)].append(s.charAt(i));
+            rows[i] = new ArrayList<>();
         }
 
-        for (int i = 1; i < numRows; i++) {
-            stacks[0].append(stacks[i]);
+        for (char c : s.toCharArray()) {
+            rows[idx].add(c);
+            if (idx == 0) {
+                d = 1;
+            } else if (idx == numRows - 1) {
+                d = -1;
+            }
+            idx += d;
         }
 
-        return stacks[0].toString();
-    }
+        StringBuilder result = new StringBuilder();
+        for (List<Character> row : rows) {
+            for (char c : row) {
+                result.append(c);
+            }
+        }
 
-    int cycle(int i, int n) {
-        int cycle = (n - 1) * 2;
-        int pos = i % cycle;
-
-        return pos < n ? pos : cycle - pos;
+        return result.toString();        
     }
 }
