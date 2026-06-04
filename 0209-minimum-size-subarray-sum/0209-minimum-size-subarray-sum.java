85393807344c7b1f15634527c545f7df6dbad53c
김현태 <118068099+HyeonTee@users.noreply.github.com>
2026-06-04 10:32:41 +0900

    Time: 1 ms (99.96%), Space: 62.1 MB (30.72%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,40 +1,18 @@
 class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-        if (nums.length == 1) {
-            return nums[0] >= target ? 1 : 0;
-        }
+      public int minSubArrayLen(int target, int[] nums) {
+          int answer = Integer.MAX_VALUE;
+          int sum = 0;
+          int lt = 0;
 
-        if (nums[0] >= target) {
-            return 1;
-        }
+          for (int rt = 0; rt < nums.length; rt++) {
+              sum += nums[rt];
+              while (sum >= target) {           // 조건 만족하면 최대한 왼쪽을 줄인다
+                  answer = Math.min(answer, rt - lt + 1);
+                  sum -= nums[lt];
+                  lt++;
+              }
+          }
 
-        int answer = nums.length + 1;
-        int lt = 0;
-        int rt = 1;
-        int sum = nums[0] + nums[1];
-        
-        while (lt < rt) {
-            if (nums[rt] >= target) {
-                return 1;
-            }
-
-            if (sum >= target) {
-                answer = Math.min(answer, rt - lt + 1);
-            }
-
-            if (rt < nums.length - 1 && (sum < target || rt == lt + 1)) {
-                rt++;
-                sum += nums[rt];
-            } else {
-                sum -= nums[lt];
-                lt++;
-            }
-        }
-
-        if (answer == nums.length + 1) {
-            return 0;
-        }
-
-        return answer;
-    }
-}
+          return answer == Integer.MAX_VALUE ? 0 : answer;
+      }
+  }
